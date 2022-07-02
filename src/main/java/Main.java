2edffefb213cff684246133a047e59b140ea54e3
the-Azrael <tusaev.maxim@yandex.ru>
2022-07-02 18:40:23 +0400

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static final String SERVER_HOST = "localhost";
+    public static final int SERVER_PORT = 20000;
+
+    public static void main(String[] args) {
+        Runnable serverTask = () -> {
+            try {
+                ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out.println("New connection accepted");
+                String name = in.readLine();
+                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        };
+
+        var clientTask = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Socket clientSocket = new Socket(SERVER_HOST, SERVER_PORT);
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    out.println("client");
+                    String resp = in.readLine();
+                    System.out.println(resp);
+                    resp = in.readLine();
+                    System.out.println(resp);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+        Thread serverThread = new Thread(serverTask);
+        serverThread.start();
+        Thread clientThread = new Thread(clientTask);
+        clientThread.start();
+    }
+}
