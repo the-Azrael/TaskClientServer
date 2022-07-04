fa5f19a63ed401c65434221483fc7a06b8e883ab
the-Azrael <tusaev.maxim@yandex.ru>
2022-07-04 18:12:54 +0400

    add try-with-resources

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Main {
     public static final String SERVER_HOST = "localhost";
@@ -8,26 +9,24 @@ public class Main {
 
     public static void main(String[] args) {
         Runnable serverTask = () -> {
-            try {
-                ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
-                Socket clientSocket = serverSocket.accept();
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+                 Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
                 out.println("New connection accepted");
                 String name = in.readLine();
                 out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
             } catch (IOException e) {
-                System.out.println(e.getMessage());
+                throw new RuntimeException(e);
             }
         };
 
         var clientTask = new Runnable() {
             @Override
             public void run() {
-                try {
-                    Socket clientSocket = new Socket(SERVER_HOST, SERVER_PORT);
+                try(Socket clientSocket = new Socket(SERVER_HOST, SERVER_PORT);
                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))  {
                     out.println("client");
                     String resp = in.readLine();
                     System.out.println(resp);
